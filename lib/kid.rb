c4c5f18f0df49541cf9e595f70008c9f8c5a4efd
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-27 22:34:59 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,8 +1,10 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid
-include Dance
+
+  include Dance
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
@@ -11,6 +13,5 @@ include Dance
   def initialize(name)
     @name = name
   end
-
 
 end
